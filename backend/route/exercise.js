4a854061545edfef443730c42e248d7bcb251fd6
firdous alam <firdous.alam2058@gmail.com>
2022-06-14 20:29:58 +0530

    added nodejs mongodb crud functionality

--- backend/route/exercise.js
+++ backend/route/exercise.js
@@ -5,13 +5,42 @@ router.route("/").get((req,res)=>{
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json('ERROR :'+err));
 });
+
+router.route("/:id").get((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercises => res.json(exercises))
+    .catch(err => res.status(400).json('ERROR :'+err));
+});
+
+router.route("/:id").delete((req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("exercise deleted"))
+    .catch(err => res.status(400).json('ERROR :'+err));
+});
+
+router.route("/update/:id").post((req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercise=>{
+        
+        exercise.username = req.body.username;
+        exercise.description= req.body.description;
+        exercise.duration= Number(req.body.duration);
+        exercise.date= Date.parse(req.body.date);
+        exercise.save()
+        .then(()=> res.json('Exercise Updated'))
+        .catch(err => res.status(400).json('ERROR :'+err));
+    })
+    .catch(err => res.status(400).json('ERROR :'+err));
+});
+
+
 router.route("/add").post((req,res)=>{
     const username= req.body.username;
     const description= req.body.description;
     const duration= Number(req.body.duration);
     const date= Date.parse(req.body.date);
-    
-    const newExercise = new Exercise({userName
+
+    const newExercise = new Exercise({username
     ,description,duration,date});
     newExercise.save()
     .then(()=> res.json('newExercise added'))
