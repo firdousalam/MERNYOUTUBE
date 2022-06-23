c9161c6b73d3e8427205db0e6750cc3b26251c21
firdous alam <firdous.alam2058@gmail.com>
2022-06-23 21:16:07 +0530

    Create UserForm

--- frontend/src/components/CreateUser.component.js
+++ frontend/src/components/CreateUser.component.js
@@ -1,11 +1,54 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class CreateUser extends Component{
-    render(){
-        return(
-            <div> 
-                Create User
+export default class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+
+        }
+    }
+    onChangeUserName(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const user = {
+            username: this.state.username
+        }
+        console.log(user);
+        alert("data submitted")
+        //  window.location = "/";
+    }
+    render() {
+        return (
+            <div>
+                <h3>Create User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>User Name : </label>
+                        <input type="text" required
+                            className='form-control'
+                            value={this.state.username}
+                            onChange={this.onChangeUserName} />
+                    </div>
+
+
+                    <div className='form-group'>
+
+                        <input type="submit"
+                            value="Create User" className='btn btn-primary' />
+
+                    </div>
+
+                </form>
             </div>
         )
     }
