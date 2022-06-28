b9bef3c0692a74b726feea942d4fb420b10b10a7
firdous alam <firdous.alam2058@gmail.com>
2022-06-28 22:12:57 +0530

    New React Typescript Project

--- frontendts/src/App.tsx
+++ frontendts/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+let cssStyle = {
+  'color' :'blue'
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+     <h1 style={{color : 'red'}}>Use Inline CSS</h1>
+     <h2 style={cssStyle}>Internal CSS Style</h2>
+     <h2 className='colors'>External CSS Style it will read stryle from app.css file</h2>
+    </React.Fragment>
   );
 }
 
