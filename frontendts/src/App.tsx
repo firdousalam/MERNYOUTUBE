083bbae74d6b5dca1f9fe2b52850e2abcc7b2a43
firdous alam <firdous.alam2058@gmail.com>
2024-02-04 16:19:15 +0530

    code update

--- frontendts/src/App.tsx
+++ frontendts/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import Profile from "./components/profile"
 function App() {
 let cssStyle = {
   'color' :'blue'
 }
   return (
     <React.Fragment>
-     <h1 style={{color : 'red'}}>Use Inline CSS</h1>
-     <h2 style={cssStyle}>Internal CSS Style</h2>
-     <h2 className='colors'>External CSS Style it will read stryle from app.css file</h2>
-     <h2 className='text-warning'>Style from Bootstraps</h2>
-     <i className='fa fa-home'>Home</i>
+     <h1> Hello world</h1>
+     < Profile name="firdous alam"></Profile>
     </React.Fragment>
   );
 }
---
+++ frontendts/src/components/profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = (props : {name : string}) => {
+  return (
+    <div>
+      i am profile components {props.name}
+    </div>
+  )
+}
+
+export default Profile
